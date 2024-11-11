3b774470b58f0f57aefec516d6cf67bac90af0e3
VK-Garg7 <14garg04@gmail.com>
2024-11-12 01:29:12 +0530

    main file

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,8 +1,15 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Route,RouterProvider,createBrowserRouter , createRoutesFromElements } from 'react-router-dom'
+import Github,{githubInfoLoader} from './components/Github/Github.jsx'
+import Layout from './components/Layout/Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -10,9 +17,18 @@ const router = createBrowserRouter(
     <Route path='' element={<Home/>}/>
     <Route path='/about' element={<About/>}/>
     <Route path='/contact' element={<Contact/>}/>
-    <Route path='/user/:userid' element={<user/>}/>
+    <Route path='/user/:userid' element={<User/>}/>
     <Route 
-    loader = {githubInfoLoader}{path='/' element={<Layout/>}/>
+    loader = {githubInfoLoader}
+    path='github'
+     element={<Github/>}
+     />
     </Route>
   )
+)
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
