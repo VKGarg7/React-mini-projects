9b03ce4367ca99932f03d38e88b992e80875e1f9
VK-Garg7 <14garg04@gmail.com>
2024-11-12 00:58:43 +0530

    github page

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Route,RouterProvider,createBrowserRouter , createRoutesFromElements } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+    <Route path='' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+    <Route path='/user/:userid' element={<user/>}/>
+    <Route 
+    loader = {path='/' element={<Layout/>}/>
+    </Route>
+  )
 )
